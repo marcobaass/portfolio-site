0856620e332f30f62eb71bfac7f693cee5f5287a
Marco Baass <picture@marcobaass.com>
2024-07-04 07:27:11 +0200

    portfolio page done

--- SCRIPT/portfolio-hamburger.js
+++ SCRIPT/portfolio-hamburger.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
+  const icon = document.querySelector('i');
 
   hamburger.addEventListener('click', () => {
     const menu = document.getElementById('menu');
     const expanded = hamburger.getAttribute('aria-expanded') === 'true';
 
     hamburger.setAttribute('aria-expanded', !expanded);
+
     menu.hidden = !menu.hidden;
+
     hamburger.setAttribute('aria-label', expanded ? 'Open menu' : 'Close menu');
+
+    if (expanded) {
+      icon.classList.remove('fa-times');
+      icon.classList.add('fa-bars');
+    } else {
+      icon.classList.remove('fa-bars');
+      icon.classList.add('fa-times');
+    }
   });
 });
