fd95f2c1f54f87a2f48f35407dce913f56af5223
Marco Baass <picture@marcobaass.com>
2024-07-07 12:41:10 +0200

    zoom effekt

--- SCRIPT/photo-scroll.js
+++ SCRIPT/photo-scroll.js
@@ -1,6 +1,8 @@
 const images = document.querySelectorAll("[data-src]");
 console.log("Number of images found:", images.length);
 
+// lazyload images
+
 function preloadImage(img) {
   const src = img.getAttribute("data-src");
   if(!src) {
@@ -11,7 +13,7 @@ function preloadImage(img) {
 
 const imgOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -650px 0px"
+  rootMargin: "0px 0px 0px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
@@ -21,7 +23,7 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
     } else {
       console.log("Loading image:", entry.target.getAttribute("data-src"));
       preloadImage(entry.target);
-      entry.target.classList.add('fullsize');
+      // entry.target.classList.add('fullsize');
       imgObserver.unobserve(entry.target);
     }
   })
@@ -30,3 +32,27 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
   imgObserver.observe(image);
 });
+
+//zoom in images
+
+const zoomOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 0px 0px"
+};
+
+const zoomObserver = new IntersectionObserver ((entries, zoomObserver) => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      entry.target.classList.add('fullsize');
+      zoomObserver.unobserve(entry.target)
+    }
+  })
+}, zoomOptions);
+
+images.forEach(image => {
+  zoomObserver.observe(image);
+});
+
