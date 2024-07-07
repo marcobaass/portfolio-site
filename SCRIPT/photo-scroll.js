d6745374ca47b6333017b4aad33371435984f468
Marco Baass <picture@marcobaass.com>
2024-07-07 09:15:22 +0200

    second intersectionObserver needed

--- SCRIPT/photo-scroll.js
+++ SCRIPT/photo-scroll.js
@@ -1,4 +1,5 @@
 const images = document.querySelectorAll("[data-src]");
+console.log("Number of images found:", images.length);
 
 function preloadImage(img) {
   const src = img.getAttribute("data-src");
@@ -9,17 +10,18 @@ function preloadImage(img) {
 };
 
 const imgOptions = {
-  threshold: 1.0,
-  rootMargin: "0px 0px -500px 0px"
+  threshold: 0,
+  rootMargin: "0px 0px -650px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
   entries.forEach(entry => {
-    console.log(`Entry:`, entry);
     if(!entry.isIntersecting ) {
       return;
     } else {
+      console.log("Loading image:", entry.target.getAttribute("data-src"));
       preloadImage(entry.target);
+      entry.target.classList.add('fullsize');
       imgObserver.unobserve(entry.target);
     }
   })
