1868b19fffa3ea40ec7e879548d3ce63f519d965
Marco Baass <picture@marcobaass.com>
2024-07-14 15:05:23 +0200

    footer start

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const spans = document.querySelectorAll('.typo');
   const myName = ['M', 'A', 'R', 'C', 'O', 'B', 'A', 'ß'];
+  let isFixedCount = 0; // Added a counter to track fixed characters
 
   async function animateCharacter(span, char, index) {
-    let isFixed = false; // Flag to track if the character is fixed
+    let isFixed = false;
+    let i = 0;
 
-    // Initial random character assignment
     let intervalId = setInterval(() => {
+      if (i < 30) {
+        i++;
+      } else {
+        clearInterval(intervalId);
+        span.textContent = char;
+        isFixed = true;
+        isFixedCount++;
+        if (isFixedCount === myName.length) {
+          startRandomAnimations(); // Call function to start random animations after all characters are fixed
+        }
+      }
+
       if (!isFixed) {
         const randomChar = getRandomUppercaseChar();
         if (randomChar !== char) {
@@ -15,6 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
           clearInterval(intervalId);
           span.textContent = char;
           isFixed = true;
+          isFixedCount++;
+          if (isFixedCount === myName.length) {
+            startRandomAnimations(); // Call function to start random animations after all characters are fixed
+          }
         }
       }
     }, 50);
@@ -25,10 +42,42 @@ document.addEventListener("DOMContentLoaded", () => {
       await animateCharacter(spans[i], myName[i], i);
     }
   })();
+
+  async function animateRandomCharacter(span, char) {
+    i = 0
+
+    let intervalId = setInterval(() => {
+        const randomChar = getRandomUppercaseChar();
+        if (randomChar !== char && i < 10) {
+          span.textContent = randomChar;
+          i++;
+        } else {
+          clearInterval(intervalId);
+          span.textContent = char;
+        }
+
+    }, 50); // Adjust the interval time if needed
+  }
+
+  function chooseRandomCharacter() {
+    const i = Math.floor(Math.random() * spans.length);
+    const span = spans[i];
+    const char = myName[i];
+    animateRandomCharacter(span, char);
+  }
+
+  function startRandomAnimations() {
+    setInterval(chooseRandomCharacter, getRandomTimeInterval()); // Start random animations at intervals
+  }
+
+  function getRandomTimeInterval() {
+    // Return a random interval between 1 and 5 seconds
+    return Math.floor(Math.random() * 2000) + 1000;
+  }
 });
 
 function getRandomUppercaseChar() {
   let randomCharCode = Math.floor(Math.random() * 27) + 65;
-  if(randomCharCode === 91) {randomCharCode = 223};
+  if (randomCharCode === 91) { randomCharCode = 223 };
   return String.fromCharCode(randomCharCode);
 }
